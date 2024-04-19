6aa6cfd0a0bd4a3bc7afe242a97b73cda3876908
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2024-04-19 06:16:08 +0500

    Elaborate the arguments validation and error messages

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,13 @@
 #!/usr/bin/env node
 import { argv } from 'node:process';
 import App from '../app.js';
-import { isUnique } from '../helpers/helpers.js';
+import { validateArgs } from '../helpers/helpers.js';
 
 const main = () => {
   const moves = argv.slice(2);
-  if (moves.length % 2 === 1 && moves.length > 1 && isUnique(moves)) {
+  if (validateArgs(moves)) {
     const app = new App(moves);
     app.welcome();
-  } else {
-    const error = Error('Please enter the odd number of arguments.');
-    console.log(error.message);
   }
 };
 
--- helpers/helpers.js
+++ helpers/helpers.js
@@ -1,2 +1,20 @@
 export const sleep = (ms = 500) => new Promise((r) => setTimeout(r, ms));
+
 export const isUnique = (array) => (new Set(array)).size == array.length;
+
+export const validateArgs = (args) => {
+  const example = "Example: 'play rock Spock paper lizard scissors'";
+  if (args.length < 3) {
+    const error = Error(`Please provide at least 3 arguments.\n${example}`);
+    console.log(error.message);
+  } else if (!isUnique(args)) {
+    const error = Error(`Please don't provide duplicate arguments.\n${example}`);
+    console.log(error.message);
+  } else if (args.length % 2 === 0) {
+    const error = Error("Please enter the odd number of arguments.\nExample: 'play rock Spock paper lizard scissors'");
+    console.log(error.message);
+  } else {
+    return true;
+  }
+  return false;
+};
